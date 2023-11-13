a667f923f21e0bdce20f424a799c26cc038c3b92
parMaster <tony.racomore@gmail.com>
2023-11-13 21:25:51 +0200

    v2 has tiny1616 chip

---
+++ hw/wurkkos/ts21v2/anduril.h
@@ -0,0 +1,47 @@
+// Wurkkos TS21 config options for Anduril using the Attiny1616
+// avrdude -c jtag2updi -P /dev/ttyUSB0 -p t1616 -v -u -Uflash:w:anduril.wurkkos-ts21v2.hex
+// Copyright (C) 2021-2023 (FIXME)
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+// same as  the  BLF Q8, mostly
+#include "sofirn/blf-q8-t1616/anduril.h"
+
+// voltage readings were a little high with the Q8 value
+#undef VOLTAGE_FUDGE_FACTOR
+#define VOLTAGE_FUDGE_FACTOR 5  // add 0.25V, not 0.35V
+
+// off mode: high (2)
+// lockout: high (2)
+#ifdef INDICATOR_LED_DEFAULT_MODE
+#undef INDICATOR_LED_DEFAULT_MODE
+#endif
+#define INDICATOR_LED_DEFAULT_MODE ((2<<2) + 2)
+
+// don't blink during the ramp; the button LED brightness is sufficient
+// to indicate which power channel(s) are being used
+#ifdef BLINK_AT_RAMP_MIDDLE
+#undef BLINK_AT_RAMP_MIDDLE
+#endif
+#ifdef BLINK_AT_RAMP_CEIL
+#undef BLINK_AT_RAMP_CEIL
+#endif
+
+// stop panicking at ~60% power
+#ifdef THERM_FASTER_LEVEL
+#undef THERM_FASTER_LEVEL
+#endif
+#define THERM_FASTER_LEVEL 105
+
+// Don't go too high in simple ui mode
+#undef SIMPLE_UI_FLOOR
+#undef SIMPLE_UI_CEIL
+#undef SIMPLE_UI_STEPS
+#define SIMPLE_UI_FLOOR  10
+#define SIMPLE_UI_CEIL   120
+#define SIMPLE_UI_STEPS  5
+
+#undef HALFSPEED_LEVEL
+#define HALFSPEED_LEVEL 30
+#undef QUARTERSPEED_LEVEL
+#define QUARTERSPEED_LEVEL 10
